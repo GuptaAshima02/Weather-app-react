b8c7010951295e519944aae05c421bf58e7ea387
Ashima <guptaashima30@gmail.com>
2023-07-14 13:48:12 +0530

    Weather-app changes done

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTemperatureHigh, faTint, faWind } from '@fortawesome/free-solid-svg-icons';
+import img from './weather-img.png';
 
 const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState(null);
-  const [location, setLocation] = useState('Jammu');
+  const [location, setLocation] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [showError, setShowError] = useState(false);
+  const [searchClicked, setSearchClicked] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   useEffect(() => {
-    let timer;
+    setShowError(false);
+  }, []);
 
-    const fetchData = async () => {
-      setIsLoading(true);
-      setError(null);
-      setShowError(false);
+  const fetchData = async () => {
+    setIsLoading(true);
+    setError(null);
+    setShowError(false);
 
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=dda26109be2e21c827819fa96320454e&units=metric`
-        );
-        setWeatherData(response.data);
-      } catch (error) {
-        setShowError(true);
-        timer = setTimeout(() => {
-          setError('Invalid location. Please enter a valid location.');
-        }, 500);
-      }
-
-      setIsLoading(false);
-    };
-
-    if (location.trim() !== '') {
-      fetchData();
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=dda26109be2e21c827819fa96320454e&units=metric`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      setShowError(true);
+      setError('Invalid location. Please enter a valid location.');
     }
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [location]);
+    setIsLoading(false);
+  };
 
   const handleLocationChange = (e) => {
-    setLocation(e.target.value);
+    const value = e.target.value;
+    setLocation(value);
+    setSearchClicked(false);
+
+    // Check if the entered value is a number
+    setIsButtonDisabled(!isNaN(value));
+  };
+
+  const handleSearchClick = () => {
+    setSearchClicked(true);
+    fetchData();
   };
 
   return (
@@ -50,24 +55,40 @@ const WeatherApp = () => {
       <div className="header">
         <h1>Weather App</h1>
       </div>
-      <input
-        type="text"
-        className="input"
-        value={location}
-        onChange={handleLocationChange}
-      />
-      {location.trim() === '' ? (
-        <p className="error">Please enter a location.</p>
-      ) : isLoading ? (
-        <div className="loader"></div>
+      <div className="input-and-button">
+        <input
+          type="text"
+          className="input"
+          value={location}
+          onChange={handleLocationChange}
+          placeholder="Enter location"
+        />
+        <button onClick={handleSearchClick} disabled={isButtonDisabled}>
+          Search
+        </button>
+      </div>
+      {searchClicked && isLoading ? (
+        <div className="loader info"></div>
       ) : showError ? (
-        <p className="error">{error}</p>
+        <p className="error info">{error}</p>
       ) : weatherData ? (
         <div className="info">
+          <div className="image">
+            <img src={img} alt="React image" />
+          </div>
           <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Humidity: {weatherData.main.humidity}%</p>
-          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+          <p>
+            <FontAwesomeIcon icon={faTemperatureHigh} className="info-icon" />
+            Temperature: {weatherData.main.temp}°C
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faTint} className="info-icon" />
+            Humidity: {weatherData.main.humidity}%
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faWind} className="info-icon" />
+            Wind Speed: {weatherData.wind.speed} m/s
+          </p>
         </div>
       ) : null}
     </div>
